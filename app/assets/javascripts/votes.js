341371b786baa707e6fa70e86c521f011c92f279
pynkvalerian <yenping.chew90@gmail.com>
2015-09-18 10:36:57 +0800

    voting function attempted using jquery ajax, but buggy atm.

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+
+	$('a').on('click', function(e){
+		e.preventDefault();
+
+		var up_down = $(this).attr('class'); // this.class
+		var type = $(this).attr('value'); // this.value
+		var type_id = $(this).attr('id'); // this.id
+		debugger
+		$.ajax({
+			method: 'POST',
+			url: '/votes',
+			dataType: 'json',
+			data: { vote: { votable_type: type, votable_id: type_id, up_down: up_down }}
+			}).done(function(){
+				$('p#'+vote[type_id]+'.'+vote[type]+'').html(''+vote[final_votes]+'');
+			});
+	});
+
+});
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,17 +1,48 @@
 class VotesController < ApplicationController
 
 	def create
-		  	if session[:user_id].nil?
+		 if session[:user_id].nil?
 			render 'login'
 		else
-			@comment = Comment.new(comment_params)
-			@comment.user_id = session[:user_id]
-			if @comment.save
-				redirect_to Question.find(session[:question_id])
+			@vote = Vote.new(params[:vote])
+			@vote.user_id = session[:user_id]
+			if @vote.save
+				final_votes = sum_of_votes(@vote.votable_type, @vote.votable_id)
+				vote = {
+					type: @vote.votable_type,
+					type_id: @vote.votable_id,
+					final_votes: final_votes
+					}
+				vote.to_json
 			else 
 				render 'new'
 			end
 		end
 	end
 
+	def show
+		@vote = Vote.find(params[:id])
+	end
+
+	def integer(up_down)
+		if up_down.include("up")
+			return 1
+		elsif up_down.include("down")
+			return -1
+		end
+	end
+
+	def sum_of_votes(type, id)
+		votes_array = []
+		if type.include("Question")
+			Question.find(id).votes.all do |vote|
+				votes_array << integer(vote.up_down)
+			end
+		elsif type.include("Answer")
+			Answer.find(id).votes.all do |vote|
+				votes_array << integer(vote.up_down)
+			end
+		end
+		votes_array.inject(:+)
+	end
 end
